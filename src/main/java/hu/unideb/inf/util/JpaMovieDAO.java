1651dd68de02cb2086bd24ad09e9a9cfd24ab104
ZbibiWestSides <karimzribi155@gmail.com>
2020-04-29 19:12:28 +0200

    JPAMovieDAO

---
+++ src/main/java/hu/unideb/inf/util/JpaMovieDAO.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.unideb.inf.util;
+
+import java.util.List;
+import net.javaguides.hibernate.entity.Movie;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+/**
+ *
+ * @author admin
+ */
+public class JpaMovieDAO implements MovieDAO {
+
+    Session session;
+    Transaction transaction;
+
+    public JpaMovieDAO() {
+        session = HibernateUtil.getSessionFactory().openSession();
+    }
+
+    @Override
+    public void close(){
+        session.close();
+        System.out.println("DAO closed...");
+    }
+
+    public void saveMovie(Movie a) {
+        try {
+            transaction = session.beginTransaction();
+            session.save(a);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+
+    public void deleteMovie(Movie a) {
+        try {
+            transaction = session.beginTransaction();
+            session.remove(a);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
+    
+    public void updateMovie(Movie a) {
+        try {
+            transaction = session.beginTransaction();
+            session.update(a);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        }
+    }
